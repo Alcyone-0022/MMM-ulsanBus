0675c753c364e7b0fc3118db03da11878e9bc008
hsk22000 <hsk22000@naver.com>
2021-12-18 16:09:24 +0900

    요소 배치 수정

--- MMM-ulsanBus.js
+++ MMM-ulsanBus.js
@@ -136,10 +136,6 @@ Module.register("MMM-ulsanBus", {
 					arrivesSoonElem.id = 'arrives_soon';
 					//CSS로 빼자
 					arrivesSoonElem.innerHTML = '곧 도착: ' + self.busStopData['arrives_soon'];
-					// arrivesSoonElem.style.fontWeight = 'bold';
-					// arrivesSoonElem.style.width = '180px';
-					// arrivesSoonElem.style.color = '#ff5733';
-					// arrivesSoonElem.style.wordBreak = 'break-word';
 
 					arrivalRouteElem.append(arrivesSoonElem);
 
